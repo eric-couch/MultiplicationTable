c1771660fef35efe976b5042a5f97d3832022179
Eric Couch <ericcouch@yahoo.com>
2023-07-10 15:43:51 -0500

    Add project files.

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter ceiling of multiplication table: ");
+            int ceiling = int.Parse(Console.ReadLine());
+            if (ceiling > 10)
+            {
+                Console.WriteLine("that's too big!");
+            } else
+            {
+                Console.Write("\t");
+                for (int col = 0; col <= ceiling; col++)
+                {
+                    Console.Write($"{col}\t");
+                }
+                Console.WriteLine("");
+                for (int i = 0; i < ceiling; i++)
+                {
+                    Console.Write($"{i}\t");
+                    for (int j = 0; j <= ceiling; j++)
+                    {
+                        Console.Write($"{i * j}\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            
+        }
+    }
+}
